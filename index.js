8b3f0d9a4567254bd89f51a01f97ed38ac0e23f8
Alvin kipkirui yaem <alvinyaem4@gmail.com>
2023-04-07 10:51:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const numSet = new Set();
+
+  for (let num of array) {
+    const complement = target - num;
+    if (numSet.has(complement)) {
+      return true;
+    }
+    numSet.add(num);
+  }
+
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  Time complexity: O(n), where n is the length of the input array.
 */
 
 /* 
   Add your pseudocode here
+  1. Create a new Set to keep track of numbers
+  2. Iterate through the array
+  3. For each number, calculate its complement (target - num)
+  4. Check if the complement is already in the Set
+  5. If yes, return true, as a pair of numbers that add up to the target has been found
+  6. If no, add the current number to the Set
+  7. If the loop finishes without finding a pair of numbers, return false
 */
 
 /*
   Add written explanation of your solution here
+  The hasTargetSum function uses a Set to keep track of numbers in the input array. It iterates through the array and calculates the complement of each number with respect to the target sum. If the complement is already present in the Set, that means a pair of numbers that add up to the target sum has been found, and the function returns true. If the loop finishes without finding a pair of numbers, the function returns false.
 */
 
 // You can run `node index.js` to view these console logs
